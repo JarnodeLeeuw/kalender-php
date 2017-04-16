b987789409c80cb0635611f2b15b05aa0769eabf
Jarno de Leeuw <9902732@mydavinci.nl>
2017-04-16 13:38:52 +0200

    Some stuff

--- framework-php-master/view/birthday/edit.php
+++ framework-php-master/view/birthday/edit.php
@@ -1,16 +1,12 @@
 <div class="container">
 	<h1>Edit</h1>
-	<form action="<?= URL ?>student/editSave" method="post">
+	<form action="<?= URL ?>Calendar/editSave" method="post">
 	
-		<input type="text" name="firstname" value="<?= $student['student_firstname']; ?>">
-		<input type="text" name="lastname" value="<?= $student['student_lastname']; ?>">
-		
-		<select name="gender">
-			<option value="male" <?php if ($student['student_gender'] == "male") { echo "selected=\"true\""; } ?>>Male</option>
-			<option value="female" <?php if ($student['student_gender'] == "female") { echo "selected=\"true\""; } ?>>Female</option>	
-		</select>
-
-		<input type="hidden" name="id" value="<?= $student['student_id']; ?>">
+		<input type="text" name="person" value="<?= $birthdays['birthdays_person']; ?>">
+		<input type="text" name="day" value="<?= $birthdays['birthdays_day']; ?>">
+		<input type="text" name="day" value="<?= $birthdays['birthdays_month']; ?>">
+		<input type="text" name="day" value="<?= $birthdays['birthdays_year']; ?>">
+		<input type="hidden" name="id" value="<?= $birthdays['birthdays_id']; ?>">
 		<input type="submit" value="Verzenden">
 	
 	</form>
--- framework-php-master/view/birthday/index.php
+++ framework-php-master/view/birthday/index.php
@@ -3,12 +3,18 @@
 	<tr>
 		<th>#</th>
 		<th>Name</th>
+		<th>Day</th>
+		<th>Month</th>
+		<th>Year</th>
 		<th colspan="2">Actions</th>
 	</tr>
 	<?php foreach ($birthdays as $birthday) { ?>
 	<tr>
 		<td><?=$birthday['id']; ?></td>
 		<td><?=$birthday['person']; ?></td>
+		<td><?=$birthday['day']; ?></td>
+		<td><?=$birthday['month']; ?></td>
+		<td><?=$birthday['year']; ?></td>
 		<td><a href="=URL . 'birthday/edit' . #birthday['id']">Edit</a></td>
 		<td><a href="=URL . 'birthday/delete' . #birthday['id']">Delete</a></td>
 	</tr>
@@ -17,5 +23,8 @@
 </table>
 
 <form>
-	<input type="text" name="birthday" placeholder="birthday name..">
+	<input type="text" name="person" placeholder="person name..">
+	<input type="text" name="day" placeholder="day number">
+	<input type="text" name="month" placeholder="month number">
+	<input type="text" name="year" placeholder="year number">
 </form>
